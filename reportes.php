9bc93f9c58f9743f41ff6eee6d5beea5dece6d48
BrendaValdovinos17 <desarrollo2@ascparts.com>
2016-08-18 17:58:31 -0500

    Reportes (10%)
    avance de reportes

--- reportes.php
+++ reportes.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
+<script src="jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="js/reportes.js"></script>
 <head>
     <style>
         body {
@@ -22,36 +24,40 @@
     <link rel="stylesheet" type="text/css" href="css/formulario.css">
 
 </head>
+
 <body>
-
-<div id="myModal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-        <span class="close">×</span>
-        <p>Filtrar por:</p>
-        <input type="text" placeholder="Cliente" class="buscar">
-        <input type="date" placeholder="Fecha" class="fecha">
-        <input type="text" placeholder="Saldo minimo" class="buscar">
-        <input type="text" placeholder="Saldo maximo" class="buscar">
-        <input type="text" placeholder="Zona" class="buscar">
-        <input type="text" placeholder="Zona" class="buscar"><br><br>
-        <div class="boton"><input type="button" value="Responder" class="colorgreen"></div>
-    </div>
-</div>
-
-
 <div class="principal">
     <div class="lineaDivisora">Reportes</div>
     <div>
         <input type="text" placeholder="Buscar Reporte" class="buscar">
-        <button id="myBtn">&#9656;Facturas por forma de pago</button>
     </div>
     <div class="reportes">
         <table align="center" >
 
+
+
+
+
+
             <tr style="background-color: white; border: 1px gray solid" ><td><div class="nombre"><label>&#9656;</label>Pedidos facturados por mes</div></td></tr>
-            <tr><td><div class="nombre">&#9656;Antigüedad de saldos</div></td></tr>
+            <tr><td>
+                    <a id="nivel1">&#9656;Antigüedad de saldos</a>
+                    <div id="nivel2" class="filtros">
+
+                        Filtrar por:
+                        <input type="text" placeholder="Cliente" class="buscar">
+                        <input type="date" placeholder="Fecha" class="fecha">
+                        <br><br>
+                        <div >
+                            <input type="button" value="Aceptar" onclick="location.href='infoCliente.phhp'" class="colorgreen" style="margin-left: 2%">
+                            <input type="button" value="Cerrar" class="colorred">
+                        </div>
+                        <br>
+
+
+
+                    </div>
+             </td></tr>
             <tr><td><div class="nombre"><label>&#9656;</label>Días Cartera</div></td></tr>
             <tr><td><div class="nombre" id="myBtn"><label>&#9656;</label>Estado de Cuenta de Cliente</div></td></tr>
             <tr><td><div class="nombre"><label>&#9656;</label>Aplicación de anticipos detallados</div></td></tr>
@@ -62,6 +68,5 @@
         </table>
     </div>
     </div>
-   <script type="text/javascript" src="js/modal.js"></script>
 </body>
 </html>
