c55093b22512890804bade6ebbb52e1f7cc3194e
BrendaValdovinos17 <desarrollo2@ascparts.com>
2016-08-26 16:13:57 -0500

    orden de cot
    cotizacion formato

--- modules/order/quotationOrder.php
+++ modules/order/quotationOrder.php
@@ -7,6 +7,13 @@
 <div class="MainTitle">Orden de Cotización</div>
 <div class="MainContainer">
     <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
+            <div class="divInputText" id="nivel1">
+                <input type="text" id="text1">
+                <label for="text1" onclick="mostrar()">numero de cliente</label>
+            </div>
+        </div>
+
 
         <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
             <div class="divInputText">
@@ -27,27 +34,32 @@
             <label for="text1">numero de vendedor</label>
         </div>
     </div>
-        <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">numero de cliente</label>
-            </div>
-        </div>
-
-
 
     </div>
+
+    <div id='oculto' style='display:none; border: solid 1px #F1F1F1' >
+        <h2>Datos Fiscales del Contacto</h2>
+        <h3>VICKY MONSERRAT SANTANA GARFIAS</h3>
+        <label>Clave del cliente: 19-475</label><br>
+        <label>RFC: SAGV-890501-R26</label><br>
+        <label>Domicilio: Av. Vicente Guerrero no.35 Col. Centro C.P: 4000</label><br>
+        <label>Iguala de la Indepegro</label><br>
+        <h2>Domicilios de Embarque y Sucursales</h2>
+        <h3>Clave del destino: 19-475-01</h3>
+        <label>VICKY MONSERRAT SANTANA GARFIAS</label><br>
+        <label>Domicilio: Av. Vicente Guerrero no.35 Col. Centro C.P: 4000</label><br>
+        <label>Iguala de la Indepegro</label>
+        <h3>Cuentas de correo electronico</h3>
+        <label>ventas@accecomp.com.mx</label>
+        <h4>Compras Web del Mes:  2</h4>
+        <h4>Ultima Compra Web: 17 Agosto 2016</h4>
+    </div>
+
     <div class="row">
 
-        <div class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">cliente</label>
-            </div>
-        </div>
-        <div class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <select class="divSelect">
+        <div class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div c class="divSelect">
+                <select>
                     <option>Cliente Frecuente</option>
                     <option>Cliente Distinguido</option>
                     <option>Cliente Excelente</option>
@@ -55,22 +67,25 @@
                 <label for="text1">grupo de cliente</label>
             </div>
         </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
             <div class="divInputText">
                 <input type="text" id="text1">
-                <label for="text1">sku</label>
+                <label for="text1">precio referencia</label>
             </div>
         </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">numero de parte</label>
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div class="divSelect">
+                <select>
+                    <option>dolar</option>
+                    <option>peso</option>
+                </select>
+                <label for="text1">moneda</label>
             </div>
         </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
             <div class="divInputText">
                 <input type="text" id="text1">
-                <label for="text1">cantidad</label>
+                <label for="text1">tipo de cambio</label>
             </div>
         </div>
 
@@ -78,10 +93,16 @@
     </div>
     <div class="row">
 
-
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
+        <div class="SubTitle">Informacion de Sku a cotizar</div>
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
             <div class="divInputText">
-                <select class="divSelect">
+                <input type="text" id="text1">
+                <label for="text1">sku</label>
+            </div>
+        </div>
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div class="divSelect">
+                <select >
                     <option>MOBILES</option>
                     <option>PROJECTORS</option>
                     <option>COPIERS</option>
@@ -94,9 +115,9 @@
                 <label for="text1">familia</label>
             </div>
         </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <select class="divSelect">
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div class="divSelect">
+                <select >
                     <option>CONCEPTRONIC</option>
                     <option>STP</option>
                     <option>ACER</option>
@@ -123,9 +144,9 @@
             </div>
         </div>
 
-          <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <select class="divSelect">
+          <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div class="divSelect">
+                <select >
                     <option>	HDD INT             	</option>
                     <option>	COMPUTER BAGS       	</option>
                     <option>	HDD EXT             	</option>
@@ -163,61 +184,44 @@
                 <label for="text1">grupo</label>
             </div>
         </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <select class="divSelect">
-                    <option>Original</option>
-                    <option>Generico</option>
-                    <option>refurbished</option>
-                </select>
-                <label for="text1">condicion</label>
-            </div>
-        </div>
-
+    </div>
+    <div class="row">
         <div class="col-lg-1 col-md-1 col-sm-2 col-xs-1">
             <div class="divInputTextArea">
                 <label for="textarea10">Descripcion</label>
-                <textarea id="textarea10"></textarea>
+                <textarea id="textarea10"></textarea></div>
+        </div>
+    </div>
+    <div class="row">
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div class="divInputText">
+                <input type="text" id="text1">
+                <label for="text1">numero de parte</label>
             </div>
         </div>
-
-
-
-    </div>
-
-    <div class="row">
-
-
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <div class="divInputText">
+                <input type="text" id="text1">
+                <label for="text1">cantidad</label>
+            </div>
+        </div>
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
             <div class="divInputText">
                 <input type="text" id="text1">
                 <label for="text1">numero de almacen</label>
             </div>
         </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">moneda</label>
-            </div>
-        </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">tipo de cambio</label>
-            </div>
-        </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">precio referencia</label>
-            </div>
-        </div>
-        <div  class="col-lg-5 col-md-5 col-sm-3 col-xs-1">
+
+
+
+        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
             <div class="divInputText">
                 <input type="text" id="text1">
                 <label for="text1">fecha promesa</label>
             </div>
         </div>
+
+
 
 
     </div>
@@ -226,6 +230,10 @@
 
     <br style="clear: both;" />
 </div>
-
+<script type="text/javascript" src="../../js/reportes.js"></script>
+<script type="text/javascript">
+    function mostrar(){
+        document.getElementById('oculto').style.display = 'block';}
+</script>
 </body>
 </html>
