de115f0abc490cae1cf3c6eca607f91286e62034
ascdesarrollo1 <desarrollo1@ascparts.com>
2016-08-26 12:07:47 -0500

    Primeros cambios para generar clases de grid y inputs

--- modules/test/form.php
+++ modules/test/form.php
@@ -62,6 +62,85 @@
 
         </div>
 
+        <div class="SubTitle">Titulo de textareas</div>
+        <div class="row">
+
+            <!-- 4 -->
+            <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea1">Este es mi text area</label>
+                    <textarea id="textarea1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea2">Este es mi text area</label>
+                    <textarea id="textarea2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea3">Este es mi text area</label>
+                    <textarea id="textarea3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea4">Este es mi text area</label>
+                    <textarea id="textarea4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <!-- 3 -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea5">Este es mi text area</label>
+                    <textarea id="textarea5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea6">Este es mi text area</label>
+                    <textarea id="textarea6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea7">Este es mi text area</label>
+                    <textarea id="textarea7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <!-- 2 -->
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea8">Este es mi text area</label>
+                    <textarea id="textarea8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea9">Este es mi text area</label>
+                    <textarea id="textarea9">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+            <!-- 1 -->
+            <div class="col-lg-1 col-md-2 col-sm-2 col-xs-1">
+                <div class="divInputTextArea">
+                    <label for="textarea10">Este es mi text area</label>
+                    <textarea id="textarea10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                </div>
+            </div>
+
+        </div>
+
         <br style="clear: both;" />
     </div>
 </body>
