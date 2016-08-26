cd1288e911113948911e949471d0576b956054e0
ascdesarrollo1 <desarrollo1@ascparts.com>
2016-08-26 13:10:49 -0500

    Primeros cambios para generar clases de grid y inputs datepickers y textareas

--- modules/test/form.php
+++ modules/test/form.php
@@ -2,11 +2,21 @@
 <html lang="en"  style="margin: 0; padding: 0; ">
 <head>
     <?php require_once("../../inc/sheet.inc");?>
+    <script src="../../lib/datepicker.js"></script>
+    <link type="text/css" rel="stylesheet" href="../../css/datepicker.css">
+    <script>
+        $(document).ready(function()
+        {
+            alert('entro')
+            $('#dte1').datepicker({'format': 'yyyy-mm-dd', 'startView':2, 'todayHighlight':true, 'autoclose':true, 'language':"es", 'maxViewMode':2, 'orientation':'bottom'});
+        });
+    </script>
 </head>
 <body style="margin: 0; padding: 0; ">
     <div class="MainTitle">Diseño formulario</div>
     <div class="MainContainer">
 
+        <!-- TEXT -->
         <div class="SubTitle">Titulo de 5 componentes</div>
         <div class="row">
 
@@ -44,6 +54,7 @@
 
         </div>
 
+        <!-- TEXT -->
         <div class="SubTitle">Titulo de 2 componentes</div>
         <div class="row">
 
@@ -57,6 +68,43 @@
                 <div class="divInputText">
                     <input type="text" id="text2">
                     <label for="text2">ejemplo 1</label>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- SELECT -->
+        <div class="SubTitle">Titulo de select's</div>
+        <div class="row">
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-1">
+                <div class="divSelect">
+                    <select id="cbo1"><option>Opcion 1</option><option>Opcion 2</option><option>Opcion 3</option></select>
+                    <label for="cbo1">ejemplo 1</label>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-1">
+                <div class="divSelect">
+                    <select id="cbo2"><option>Opcion 1</option><option>Opcion 2</option><option>Opcion 3</option></select>
+                    <label for="cbo2">ejemplo 1</label>
+                </div>
+            </div>
+        </div>
+
+        <!-- DATE -->
+        <div class="SubTitle">Titulo de datepicker's</div>
+        <div class="row">
+
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-1">
+                <div class="divInputText">
+                    <input type="text" id="dte1">
+                    <label for="dte1">ejemplo 1</label>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-1">
+                <div class="divInputText">
+                    <input type="text" id="dte2">
+                    <label for="dte2">ejemplo 1</label>
                 </div>
             </div>
 
@@ -132,7 +180,7 @@
             </div>
 
             <!-- 1 -->
-            <div class="col-lg-1 col-md-2 col-sm-2 col-xs-1">
+            <div class="col-lg-1 col-md-1 col-sm-2 col-xs-1">
                 <div class="divInputTextArea">
                     <label for="textarea10">Este es mi text area</label>
                     <textarea id="textarea10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
