c2fd5e00d4ff67804afbce61582b1cfd2e899acf
BrendaValdovinos17 <desarrollo2@ascparts.com>
2016-08-29 11:44:14 -0500

    orden de cotizacion
    orden 95

--- modules/order/quotationOrder.php
+++ modules/order/quotationOrder.php
@@ -19,163 +19,136 @@
 <body>
 <div class="MainTitle">Orden de Cotización</div>
 <div class="MainContainer">
+
     <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
-            <div class="divInputText" id="nivel1">
-                <input type="text" id="text1">
-                <label for="text1" onclick="mostrar()">numero de cliente</label>
-            </div>
-        </div>
-
-
-        <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
-            <div class="divInputText">
-                <input type="text" id="text1">
-                <label for="text1">folio</label>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
-            <div class="divInputText">
-                <input type="date" id="text1">
-                <label for="text1">fecha</label>
-            </div>
-        </div>
-    <div class="col-lg-4 col-md-4 col-sm-2 col-xs-1">
+    <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
         <div class="divInputText">
             <input type="text" id="text1">
-            <label for="text1">numero de vendedor</label>
+            <label for="text1" onclick="mostrar()">Cliente</label>
         </div>
     </div>
 
-    </div>
-
-    <div id="oculto" style="display:none;" >
-
-        <table width="97.3%" style="border: solid 1px #1766A1; margin-left: 15px; ">
-            <tr>
-                <td colspan="2"><div class="SubTitle">Informacion del cliente</div></td>
-            </tr>
-            <tr>
-                <td><span>Nombre del cliente: Gael Manzanares</span></td>
-                <td><span>Domicilio: Pino 4567 Col. Fresno</span></td>
-            </tr>
-            <tr>
-                <td><span>condicion de pago: Contado</span></td>
-                <td><span><span>RFC: GAGV-890501-R26</span></td>
-            </tr>
-            <tr>
-                <td><span><span>Telefono: 33 33 56 78 98</span></td>
-                <td><span><span>E-mail: gael@gmail.com</span></td>
-            </tr>
-            <tr>
-                <td><span>condicion de pago: Contado</span></td>
-                <td><span><span>RFC: GAGV-890501-R26</span></td>
-            </tr>
-            <tr>
-                <td><span><span>Telefono: 33 33 56 78 98</span></td>
-                <td><span><span>E-mail: gael@gmail.com</span></td>
-            </tr>
-            <tr>
-                <td><span>Nombre del cliente: Gael Manzanares</span></td>
-                <td><span>Domicilio: Pino 4567 Col. Fresno</span></td>
-            </tr>
-        </table>
-
-    </div>
-
-    <div class="row">
-
-        <div class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
-            <div c class="divSelect">
-                <select>
-                    <option>Cliente Frecuente</option>
-                    <option>Cliente Distinguido</option>
-                    <option>Cliente Excelente</option>
-                </select>
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
+            <div class="divInputText">
+                <input type="text" id="text1">
                 <label for="text1">grupo de cliente</label>
             </div>
         </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divInputText">
                 <input type="text" id="text1">
                 <label for="text1">precio referencia</label>
             </div>
         </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
-            <div class="divSelect">
-                <select>
-                    <option>dolar</option>
-                    <option>peso</option>
-                </select>
-                <label for="text1">moneda</label>
-            </div>
-        </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divInputText">
                 <input type="text" id="text1">
                 <label for="text1">tipo de cambio</label>
             </div>
         </div>
 
-
     </div>
     <div class="row">
+        <div id="oculto" style="" >
 
-        <div class="SubTitle">Informacion de Sku a cotizar</div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+            <table width="97.3%" style="border: solid 1px #1766A1; margin-left: 15px; ">
+                <tr>
+                    <td colspan="2"><div class="SubTitle">Informacion del cliente</div></td>
+                </tr>
+                <tr>
+                    <td><span>Nombre del cliente: Gael Manzanares</span></td>
+                    <td><span>Domicilio: Pino 4567 Col. Fresno</span></td>
+                </tr>
+                <tr>
+                    <td><span>condicion de pago: Contado</span></td>
+                    <td><span><span>RFC: GAGV-890501-R26</span></td>
+                </tr>
+                <tr>
+                    <td><span><span>Telefono: 33 33 56 78 98</span></td>
+                    <td><span><span>E-mail: gael@gmail.com</span></td>
+                </tr>
+                <tr>
+                    <td><span>condicion de pago: Contado</span></td>
+                    <td><span><span>RFC: GAGV-890501-R26</span></td>
+                </tr>
+                <tr>
+                    <td><span><span>Telefono: 33 33 56 78 98</span></td>
+                    <td><span><span>E-mail: gael@gmail.com</span></td>
+                </tr>
+                <tr>
+                    <td><span>Nombre del cliente: Gael Manzanares</span></td>
+                    <td><span>Domicilio: Pino 4567 Col. Fresno</span></td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+    <div class="SubTitle">Informacion de SKU</div>
+    <div class="row">
+    <div class="col-xs-1-1 col-sm-2-2 col-md-4-5 col-lg-4-5">
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divInputText">
                 <input type="text" id="text1">
                 <label for="text1">sku</label>
             </div>
         </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
-            <div class="divSelect">
-                <select >
-                    <option>MOBILES</option>
-                    <option>PROJECTORS</option>
-                    <option>COPIERS</option>
-                    <option>PRINTERS</option>
-                    <option>LAPTOPS</option>
-                    <option>SERVERS</option>
-                    <option>DESKTOPS</option>
-                    <option>ACCESORIES</option>
-                </select>
-                <label for="text1">familia</label>
-            </div>
-        </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
-            <div class="divSelect">
-                <select >
-                    <option>CONCEPTRONIC</option>
-                    <option>STP</option>
-                    <option>ACER</option>
-                    <option>APPLE</option>
-                    <option>COMPAQ</option>
-                    <option>DELL</option>
-                    <option>FUJITSU</option>
-                    <option>GATEWAY</option>
-                    <option>MINOLTA</option>
-                    <option>EPSON</option>
-                    <option>NEC</option>
-                    <option>MITAC</option>
-                    <option>HP</option>
-                    <option>IBM</option>
-                    <option>LEXMARK</option>
-                    <option>PALM</option>
-                    <option>TOSHIBA</option>
-                    <option>PANASONIC</option>
-                    <option> OKIDATA</option>
-                    <option>XEROX</option>
 
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
+            <div class="divSelect">
+                <select id="cbo1">
+                    <option>	MOBILES             	</option>
+                    <option>	PROJECTORS          	</option>
+                    <option>	COPIERS             	</option>
+                    <option>	PRINTERS            	</option>
+                    <option>	LAPTOPS             	</option>
+                    <option>	SERVERS             	</option>
+                    <option>	DESKTOPS            	</option>
+                    <option>	ACCESORIES          	</option>
                 </select>
-                <label for="text1">marca</label>
+                <label for="cbo1">familia</label>
             </div>
         </div>
 
-          <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divSelect">
-                <select >
+                <select id="cbo1">
+                    <option>	CONCEPTRONIC	</option>
+                    <option>	STP	</option>
+                    <option>	ACER	</option>
+                    <option>	APPLE	</option>
+                    <option>	COMPAQ	</option>
+                    <option>	DELL	</option>
+                    <option>	605	</option>
+                    <option>	FUJITSU	</option>
+                    <option>	GATEWAY	</option>
+                    <option>	608	</option>
+                    <option>	MINOLTA	</option>
+                    <option>	610	</option>
+                    <option>	611	</option>
+                    <option>	612	</option>
+                    <option>	EPSON	</option>
+                    <option>	NEC	</option>
+                    <option>	MITAC	</option>
+                    <option>	HP	</option>
+                    <option>	IBM	</option>
+                    <option>	LEXMARK	</option>
+                    <option>	PALM	</option>
+                    <option>	TOSHIBA	</option>
+                    <option>	PANASONIC	</option>
+                    <option>	OKIDATA	</option>
+                    <option>	623	</option>
+                    <option>	XEROX	</option>
+                    <option>	637	</option>
+                </select>
+                <label for="cbo1">marca</label>
+            </div>
+        </div>
+
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
+            <div class="divSelect">
+                <select id="cbo1">
                     <option>	HDD INT             	</option>
                     <option>	COMPUTER BAGS       	</option>
                     <option>	HDD EXT             	</option>
@@ -209,56 +182,65 @@
                     <option>	MAINTENANCE KIT PART	</option>
                     <option>	FUSING ASSY PARTS   	</option>
                     <option>	KEYBOARDS           	</option>
+
                 </select>
-                <label for="text1">grupo</label>
+                <label for="cbo1">modelo</label>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-1 col-md-1 col-sm-2 col-xs-1">
-            <div class="divInputTextArea">
-                <label for="textarea10">Descripcion</label>
-                <textarea id="textarea10"></textarea></div>
-        </div>
-    </div>
-    <div class="row">
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divInputText">
                 <input type="text" id="text1">
-                <label for="text1">numero de parte</label>
+                <label for="text1">No. parte</label>
             </div>
         </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divInputText">
                 <input type="text" id="text1">
                 <label for="text1">cantidad</label>
             </div>
         </div>
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
+
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
             <div class="divInputText">
                 <input type="text" id="text1">
-                <label for="text1">numero de almacen</label>
+                <label for="text1">no. almacen</label>
             </div>
         </div>
 
-
-
-        <div  class="col-lg-4 col-md-4 col-sm-3 col-xs-1">
-            <div class="divInputText">
+        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
+            <div class="divInputDate">
                 <input type="date" id="text1">
                 <label for="text1">fecha promesa</label>
             </div>
         </div>
+    </div>
+    <div class="col-xs-1-1 col-md-1-1 col-md-1-5 col-md-1-5">
+        <div class="col-xs-1-1 col-sm-1-1 col-md-1-1 col-lg-1-1">
 
+            <div class="divInputTextArea">
+                <label for="textarea1">Descripcion de sku</label>
+                <textarea id="textarea1"></textarea>
+            </div>
 
-
-
+        </div>
     </div>
 
-
+</div>
+    <div class="row">
+        <div class="col-lg-1-1 col-md-1-1">
+            <div >
+                <input type="button" value="Enviar" class="colorgreen"  style="text-align: right">
+            </div>
+        </div>
+    </div>
 
     <br style="clear: both;" />
 </div>
+
+
+
 <script type="text/javascript" src="../../js/reportes.js"></script>
 <script type="text/javascript">
     function mostrar(){
--- modules/order/resolvedQuote.php
+++ modules/order/resolvedQuote.php
@@ -4,9 +4,20 @@
     <?php require_once("../../inc/sheet.inc");?>
 </head>
 <body>
-
+<div class="MainTitle">Cotizaciones Pendientes</div>
 <div class="MainContainer">
-
+    <div class="row">
+        <table border="1px soli gray" width="100%">
+            <tr>
+                <td>Folio</td>
+                <td>SKU</td>
+                <td>Cantidad</td>
+                <td>Existencia</td>
+                <td>Faltantes</td>
+            </tr>
+        </table>
+    </div>
+    <br style="clear: both;" />
 </div>
 </body>
 </html>
