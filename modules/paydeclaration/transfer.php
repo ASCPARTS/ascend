d969dbbfecd492a7223943e65f7e58cb9ac61f2c
Velkan Gael Manzanares <gaelgeovanimq@gmail.com>
2016-09-05 16:20:03 -0500

    p&pv2.2

--- modules/paydeclaration/transfer.php
+++ modules/paydeclaration/transfer.php
@@ -4,9 +4,9 @@
     <?php require_once("../../inc/sheet.inc");?>
 </head>
 <body>
-    <div class="MainTitle">FAMILIA</div>
+    <div class="MainTitle">TRASPASO DE MATERIAL</div>
     <div class="MainContainer">
-    <div class="SubTitle">REGISTRO DE FAMILIAS</div>
+    <div class="SubTitle"></div>
         <div class="row">
             <div class="col-lg-1-3 col-md-1-3 col-sm-1-2 col-xs-1-1">
                 <div class="divSelect">
