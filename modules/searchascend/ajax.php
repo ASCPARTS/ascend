0cfbe2c0c124ec422ddd95ee602a0205f5a3ed46
BrendaValdovinos17 <desarrollo2@ascparts.com>
2016-09-19 10:11:34 -0500

    filtros buscador

--- modules/searchascend/ajax.php
+++ modules/searchascend/ajax.php
@@ -65,7 +65,9 @@ switch ($strProcess)
             ."$sqlWherePartNumber "
             ."$sqlWhereDescription "
             .")
+            
             and
+            
             ("
             ."$sqlBrand "
             ."$sqlGroup "
