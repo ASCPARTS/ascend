bf19ac2d9f5e6a10ac49cadefc1ef03478963d6e
BrendaValdovinos17 <desarrollo2@ascparts.com>
2016-12-13 10:08:53 -0600

    v1 Promociones

--- modules/promotion/index.php
+++ modules/promotion/index.php
@@ -3,10 +3,13 @@
 <head>
     <?php
     require_once ('../../inc/include.config.php');
-    ini_set("display_errors",0);
     require_once('../../'. LIB_PATH .'class.ascend.php');
     $objAscend = new clsAscend();
     require_once("../../inc/sheet.inc");
+
+    ini_set("display_errors",1);
+
+
     ?>
     
 </head>
@@ -20,19 +23,16 @@
             <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4 col-lg-1-5"><div class="divSelect groupYellow"><select id="strStatus"><option value="'A'" selected="selected">Activas</option><option value="'C'">Canceladas</option><option value="'A','C'">Ambas</option></select><label>Estatus</label></div></div>
         </div>
         <div class="ButtonContainer">
-            <button class="btn btnOnlineGreen" type="button" id="myBtn" onclick="onclick="getPromo();"">Filtrar</button>
-        </div>
-        <div class="ButtonContainer">
-            <button class="btn btnOnlineGreen" type="button" id="myBtn" onclick="getModal('myModal','closeModal')">Nueva Promoción</button>
+            <button class="btn btnOnlineGreen" type="button" id="myBtn" onclick="getPromo();">Filtrar</button>
+            <button class="btn btnOnlineGreen" type="button" id="myBtn" onclick="getProductList(0)">Nueva Promoción</button>
         </div>
         <div class="row" id="tblPromo">
        
         </div>
      
 
-        <!--ROW DEL MODAL [INICIO]-->
+        <!--MODAL NUEVA PROMOCION-->
         <div class="row">
-        
             <div id="myModal" class="modal">
 
                 <!-- Modal content -->
@@ -40,72 +40,122 @@
                     <span id="closeModal" class="close">×</span>
                     <div class="MainTitle">NUEVA PROMOCION</div>
                     <div class="MainContainer" id="newPromo">
+                        <div class="SubTitle">Información de la Promoción</div>
                         <div class="row">
-                            <div class="col-lg-1-4 col-md-1-4 col-sm-1-3 col-xs-1-2">
-                                <div class="divInfo">
-                                    <span type="text">Alfredo Aguilar</span>
-                                    <label for="text1">Usuario Creador</label>
-                                </div>
-                            </div>
-                            <div class="col-lg-1-4 col-md-1-4 col-sm-1-3 col-xs-1-2">
-                                <div class="divInfo">
-                                    <span type="text">2016-12-11</span>
-                                    <label for="text1">fecha de creación</label>
-                                </div>
-                            </div>
-                            <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4 col-lg-1-4">
-                                <div class="divInputText calendarYellow">
-                                    <input type="text" class="datepicker-here" style="cursor: pointer" onkeydown="return false;" value="<?php echo date('Y-m-d'); ?>" id="intDateFrom">
-                                    <label>Fecha (de)</label>
-                                </div>
-                            </div>
-                            <div class="col-xs-1-1 col-sm-1-2 col-md-1-3 col-md-1-4 col-lg-1-4">
-                                <div class="divInputText calendarYellow "><input type="text" class="datepicker-here" style="cursor: pointer" onkeydown="return false;" value="<?php echo date('Y-m-d'); ?>" id="intDateTo">
-                                    <label>Fecha (hasta)</label>
-                                </div>
-                            </div>
-
-
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
-                                <div class="divInputText discountYellow">
-                                    <input type="text">
-                                    <label>descuento de promocion</label>
-                                </div>
-                            </div>
-
-                            <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
                                 <div class="divInputText attachmentYellow">
-                                    <input type="text">
-                                    <label>titulo de la promoción</label>
+                                    <input type="text" id="strName">
+                                    <label>Nombre de la Promoción</label>
                                 </div>
                             </div>
-
-                            <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
-                                <div class="divSelect referenceYellow">
-                                    <select>
-                                        <option>A</option>
-                                        <option>C</option>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divSelect discountYellow">
+                                    <select id="strDiscount">
+                                        <option value="'PORCENTAJE'" selected="selected">Porcentaje</option>
+                                        <option value="'USD/MXN'">Valor Monetario</option>
+                                        <option value="'PZAS'">Piezas</option>
                                     </select>
-                                    <label for="x">Estatus</label>
+                                    <label>Tipo de Promoción</label>
                                 </div>
                             </div>
-
-
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divInputText numberYellow">
+                                    <input type="text" id="intDiscount">
+                                    <label>Cantidad a descontar</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divInputText calendarYellow">
+                                    <input type="text" class="datepicker-here" style="cursor: pointer" onkeydown="return false;" value="<?php echo date('Y-m-d'); ?>" id="intDateNewFrom">
+                                    <label>Inicio de la Promoción</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3"><div class="divInputText calendarYellow ">
+                                    <input type="text" class="datepicker-here" style="cursor: pointer" onkeydown="return false;" value="<?php echo date('Y-m-d'); ?>" id="intDateNewTo">
+                                    <label>Fin de la Promoción</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="SubTitle">Filtrar por:</div>
+                        <div class="row">
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divInputText attachmentYellow">
+                                    <input type="text" id="strSKU">
+                                    <label>SKU</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divInputText attachmentYellow">
+                                    <input type="text" id="strPartNumber">
+                                    <label>Número de Parte</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divSelect groupYellow ">
+                                    <select id="intFamily">
+                                        <option value="-1">--Seleccionar--</option>
+                                        <?php
+                                        $strSql="SELECT intId AS strValue, strName AS strDisplay FROM tblFamily WHERE strStatus = 'A' ORDER BY 2;";
+                                        $rstFamily = $objAscend->dbQuery($strSql);
+                                        foreach ($rstFamily as $arrFamily){
+                                            ?>
+                                            <option value='<?php echo $arrFamily['strValue']; ?>'><?php echo $arrFamily['strDisplay']; ?></option>
+                                            <?php
+                                        }
+                                        unset($rstFamily);
+                                        ?>
+                                    </select>
+                                    <label for="cbo1">Familia</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divSelect groupYellow ">
+                                    <select id="intBrand">
+                                        <option value="-1">--Seleccionar--</option>
+                                        <?php
+                                        $strSql="SELECT intId AS strValue, strName AS strDisplay FROM tblBrand WHERE strStatus = 'A' ORDER BY 2;";
+                                        $rstBrand = $objAscend->dbQuery($strSql);
+                                        foreach ($rstBrand as $arrBrand){
+                                            ?>
+                                            <option value='<?php echo $arrBrand['strValue']; ?>'><?php echo $arrBrand['strDisplay']; ?></option>
+                                            <?php
+                                        }
+                                        unset($rstBrand);
+                                        ?>
+                                    </select>
+                                    <label for="cbo1">Marca</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-1-5 col-md-1-5 col-sm-1-4 col-xs-1-3">
+                                <div class="divSelect groupYellow ">
+                                    <select id="intGroup">
+                                        <option value="-1">--Seleccionar--</option>
+                                        <?php
+                                        $strSql="SELECT intId AS strValue, strName AS strDisplay FROM tblGroup WHERE strStatus = 'A' ORDER BY 2;";
+                                        $rstGroup = $objAscend->dbQuery($strSql);
+                                        foreach ($rstGroup as $arrGroup){
+                                            ?>
+                                            <option value='<?php echo $arrGroup['strValue']; ?>'><?php echo $arrGroup['strDisplay']; ?></option>
+                                            <?php
+                                        }
+                                        unset($rstGroup);
+                                        ?>
+                                    </select>
+                                    <label for="cbo1">Group</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ButtonContainer">
+                            <button class="btn btnOnlineGreen" type="button" id="myBtn" onclick="getProductList(0);">Consultar</button>
                         </div>
                         <div class="row" id="divProduct">
-
-
+                            
                         </div>
 
                         <div class="ButtonContainer">
                             <input class="btn btnOnlineGreen" type="button" value="Guardar">
                         </div>
-
-                        <div class="ButtonContainer">  
-                            <input class="btn btnOnlineGreen" type="button" value="Guardar"> 
-                        </div>
+                        
                         
                         <br style="clear: both;" />
                     </div>
@@ -116,7 +166,7 @@
         </div>
         <!--ROW DEL MODAL [FIN]-->
 
-        <!--ROW DEL MODAL EDITAR[INICIO]-->
+        <!--MODAL EDITAR-->
         <div class="row">
         
             <div id="modalEditar" class="modal">
@@ -154,12 +204,22 @@
 
                         </div>
                         <div class="row">
-                            <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
-                                <div class="divInputText discountYellow">
-                                    <input type="text">
-                                    <label>descuento de promocion</label>
-                                </div>
+                            <div class="divSelect discountYellow">
+                                <select id="strStatus">
+                                    <option value="'PORCENTAJE'" selected="selected">Porcentaje</option>
+                                    <option value="'USD/MXN'">Valor Monetario</option>
+                                    <option value="'PZAS'">Piezas</option>
+                                </select>
+                                <label>Estatus</label>
                             </div>
+                        </div>
+
+                        <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
+                            <div class="divInputText numberYellow">
+                                <input type="text">
+                                <label>Cantidad a descontar</label>
+                            </div>
+                        </div>
 
                             <div class="col-xs-1-1 col-sm-1-2 col-md-1-4 col-md-1-4">
                                 <div class="divInputText attachmentYellow">
@@ -190,12 +250,16 @@
                 </div>
 
             </div>
+        <br style="clear: both;" />
         </div>
         <!--ROW DEL MODAL [FIN]-->
         <br style="clear: both;" />
     </div>
-
+<?php require_once("../../inc/include.working.php");?>
 </body>
-<script type="text/javascript" src="javascript.js"></script>
+<script>
+    $intDD = '<?php echo date('Y-m-d');?>';
+</script>
+<script src="javascript.js?v=<?php echo date('Ymdhis') ;?>"></script>
 <script type="text/javascript" src="../../js/modal.js"></script>
 </html>
--- modules/promotion/javascript.js
+++ modules/promotion/javascript.js
@@ -1,44 +1,74 @@
 $('document').ready(function(){
     $('#intDateFrom').datepicker({inline: false, language: 'es', firstDay: 1, toggleSelected: false, autoClose: true, onSelect: function(){if(parseInt($('#intDateFrom').val().split('-').join(''))>parseInt($('#intDateTo').val().split('-').join(''))){$('#intDateTo').val($('#intDateFrom').val());}}});
     $('#intDateTo').datepicker({inline: false, language: 'es', firstDay: 1, toggleSelected: false, autoClose: true, onSelect: function(){if(parseInt($('#intDateTo').val().split('-').join(''))<parseInt($('#intDateFrom').val().split('-').join(''))){$('#intDateFrom').val($('#intDateTo').val());}}});
+
+    $('#intDateNewFrom').datepicker({inline: false, language: 'es', firstDay: 1, toggleSelected: false, autoClose: true, onSelect: function(){if(parseInt($('#intDateNewFrom').val().split('-').join(''))>parseInt($('#intDateNewTo').val().split('-').join(''))){$('#intDateNewTo').val($('#intDateNewFrom').val());}}});
+    $('#intDateNewTo').datepicker({inline: false, language: 'es', firstDay: 1, toggleSelected: false, autoClose: true, onSelect: function(){if(parseInt($('#intDateNewTo').val().split('-').join(''))<parseInt($('#intDateNewFrom').val().split('-').join(''))){$('#intDateNewFrom').val($('#intDateNewTo').val());}}});
+
     getPromo();
 });
 
 function getPromo(){
-    $strQueryString = "strProcess=promotion&intDateFrom=" + $('#intDateFrom').val().substr(0,4) + $('#intDateFrom').val().substr(5,2) + $('#intDateFrom').val().substr(8,2) + "000000&intDateTo=" + $('#intDateTo').val().substr(0,4) + $('#intDateTo').val().substr(5,2) + $('#intDateTo').val().substr(8,2) + "999999&strStatus=" + $('#strStatus').val();
-    $.ajax({
-        url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
-        success: function ($jsnPhpScriptResponse) {
-            $('#tblPromo').html('');
-            $('#tblPromo').html($jsnPhpScriptResponse.promotionList);
-        }
-    });
-}
-
-function getProduct(){
-    $strQueryString = "strProcess=product";
-    $.ajax({
-        url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
-        success: function ($jsnPhpScriptResponse) {
-            $('#divProduct').html('');
-            $('#divProduct').html($jsnPhpScriptResponse.productList);
-        }
-    });
-}
-
-
-
-
-function getProductList(){
     $('#divWorkingBackground').fadeIn('slow',function(){
-        $('#divpruebas').html('');
-        $strQueryString = "";
+        $('#tblPromo').html('');
+        $strQueryString = "strProcess=promotion&intDateFrom=" + $('#intDateFrom').val().substr(0,4) + $('#intDateFrom').val().substr(5,2) + $('#intDateFrom').val().substr(8,2) + "000000&intDateTo=" + $('#intDateTo').val().substr(0,4) + $('#intDateTo').val().substr(5,2) + $('#intDateTo').val().substr(8,2) + "999999&strStatus=" + $('#strStatus').val();
         $.ajax({
             url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
             success: function ($jsnPhpScriptResponse) {
-                $('#divpruebas').html($jsnPhpScriptResponse.tblPromoList);
+                $('#tblPromo').html($jsnPhpScriptResponse.promotionList);
                 $('#divWorkingBackground').fadeOut('slow');
+            },
+            error: function($e){
+                alert('algo paso');
+                console.log($e);
             }
         });
+    });
+}
+function getProductList($intIdPromo){
+    $('#divWorkingBackground').fadeIn('slow',function(){
+        $('#divProduct').html('');
+        if($intIdPromo==0){
+            $('#intDateNewFrom').val('$intDD');
+            $('#intDateNewTo').val('$intDD');
+            $('#strDiscount option:eq(0)').prop('selected',true);
+            //terminar de limpiar controladores
+            $strQueryString = "strProcess=newPromo&strSKU="+$('#strSKU').val()+ "&strPartNumber=" + $('#strPartNumber').val() + "&intFamily=" + $('#intFamily').val() + "&intBrand=" + $('#intBrand').val() + "&intGroup=" + $('#intGroup').val();
+
+            console.log($strQueryString);
+
+            $.ajax({
+                url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
+                success: function ($jsnPhpScriptResponse) {
+                    $('#divProduct').html($jsnPhpScriptResponse.productList);
+                }
+            });
+        }else {
+            $strQueryString = "strProcess=promodetail&intIdPromo=" + $intIdPromo;
+            $.ajax({
+                url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
+                success: function ($jsnPhpScriptResponse) {
+                   // $('#divProduct').html($jsnPhpScriptResponse.productList);
+                }
+            });
+        }
+        $('#myModal').show('fast', function () {
+            $('#divWorkingBackground').fadeOut('slow');
+        });
+    });
+}
+
+function cancelPromo($intIdPromo){
+    $('#divWorkingBackground').fadeIn('slow',function(){
+        $('#divpruebas').html('');
+            $strQueryString = "strProcess=promodetail&intIdPromo=" + $intIdPromo;
+            $.ajax({
+                url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
+                success: function ($jsnPhpScriptResponse) {
+                    $('#myModal').show('fast',function(){
+                        $('#divWorkingBackground').fadeOut('slow');
+                    });
+                }
+            });
     });
 }
