f05659728baf47c9df7acaac5941f71f1d6f67c7
BrendaValdovinos17 <desarrollo2@ascparts.com>
2016-12-17 23:35:22 -0600

    proyecciones de ventas

--- modules/salesProjection/index.php
+++ modules/salesProjection/index.php
@@ -17,7 +17,7 @@
 <div class="MainContainer">
     <div class="SubTitle">Proyección</div>
     <div class="row">
-        <div class="col-xs-1-1 col-sm-1-1 col-md-1-2 col-md-1-2 col-lg-1-2">
+        <div class="col-xs-1-1 col-sm-1-1 col-md-1-2 col-md-1-3 col-lg-1-3">
             <div class="divSelect groupYellow ">
                 <select id="decFactor">
                     <option value="-1">--Seleccionar--</option>
@@ -35,7 +35,7 @@
                 <label for="cbo1">Año</label>
             </div>
         </div>
-        <div class="col-xs-1-1 col-sm-1-1 col-md-1-2 col-md-1-2 col-lg-1-2">
+        <div class="col-xs-1-1 col-sm-1-1 col-md-1-2 col-md-1-3 col-lg-1-3">
             <div class="divSelect numberYellow">
                 <select id="strEstimate">
                     <option value="S" selected="selected">SI</option>
@@ -43,6 +43,18 @@
                 </select>
                 <label>Estimado</label>
             </div>
+        </div>
+    </div>
+    <div class="col-xs-1-1 col-sm-1-1 col-md-1-2 col-md-1-3 col-lg-1-3">
+        <div class="divSelect groupYellow ">
+            <select id="intProjection">
+                <option value="-1">--Seleccionar--</option>
+                <option value="1">ZONA</option>
+                <option value="2">VENDEDOR</option>
+                
+                ?>
+            </select>
+            <label for="cbo1">Proyeccion Por:</label>
         </div>
     </div>
     <div class="ButtonContainer">
--- modules/salesProjection/javascript.js
+++ modules/salesProjection/javascript.js
@@ -3,8 +3,9 @@ $('document').ready(function(){
 });
 function calculation(){
         $('#divWorkingBackground').fadeIn('slow',function(){
-            $strQueryString = "strProcess=salesProjection&decFactor="+$('#decFactor').val() + "&strEstimate=" + $('#strEstimate').val();
+            $strQueryString = "strProcess=salesProjection&decFactor="+$('#decFactor').val() + "&strEstimate=" + $('#strEstimate').val() + "&intProjection="+ $('#intProjection').val();
             console.log($strQueryString);
+
             $.ajax({
                 url: "ajax.php", data: $strQueryString, type: "POST", dataType: "json",
                 success: function ($jsnPhpScriptResponse) {
