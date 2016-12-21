d6c9e8df14a30c9876435d4ea1e216b3338a3b30
ASCPARTS <gerencia.sistemas@ascparts.com>
2016-12-21 17:39:47 -0600

    google callback url

--- lib/google/gpConfig.php
+++ lib/google/gpConfig.php
@@ -1,23 +1,17 @@
 <?php
-//session_start();
-
-//Include Google client library 
 include_once 'Google_Client.php';
 include_once 'contrib/Google_Oauth2Service.php';
-
-/*
- * Configuration and setup Google API
- */
 $clientId = '839294887884-aocngunfl5p38s9lmpqiobcosjingj1f.apps.googleusercontent.com'; //Google client ID
 $clientSecret = 'xhJHgeurjllQqvzIPLpiJ7KN'; //Google client secret
-$redirectURL = 'http://localhost/ascend/glogin.php'; //Callback URL
-
-//Call Google API
+if(trim(strtolower($_SERVER['HTTP_HOST']))=='localhost'){
+    $redirectURL = 'http://localhost/ascend/login/'; //Callback URL
+}else{
+    $redirectURL = 'http://demo.ascparts.com/login/'; //Callback URL
+};
 $gClient = new Google_Client();
 $gClient->setApplicationName('ascend');
 $gClient->setClientId($clientId);
 $gClient->setClientSecret($clientSecret);
 $gClient->setRedirectUri($redirectURL);
-
 $google_oauthV2 = new Google_Oauth2Service($gClient);
 ?>
